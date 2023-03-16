13093975e9b4167b99923d0e638cea0a62c82840
RaphelFrey <info@raphaelfrey.com>
2023-03-16 22:54:26 +0100

    Added basic funcionality for events and the window

--- Xenia/src/Xenia/Application.cpp
+++ Xenia/src/Xenia/Application.cpp
@@ -1,22 +1,30 @@
 #include  "xnpch.h"
 #include "Application.h"
-
 #include <GL/gl.h>
-
 #include "Log.h"
-#include "Events/ApplicationEvent.h"
 
 namespace Xenia {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
 	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		XN_CORE_TRACE("{0}", e);
+	}
+
 
 	void Application::Run()
 	{
@@ -27,5 +35,12 @@ namespace Xenia {
 			m_Window->OnUpdate();
 		}
 	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
 
 }
--- Xenia/src/Xenia/Application.h
+++ Xenia/src/Xenia/Application.h
@@ -3,6 +3,7 @@
 #include "Core.h"
 #include "Events/Event.h"
 #include "Window.h"
+#include "Events/ApplicationEvent.h"
 
 namespace Xenia {
 
@@ -12,7 +13,9 @@ namespace Xenia {
 		Application();
 		virtual ~Application();
 		void Run();
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
